6530b8f4bbed92b9aeeacf95098294041a357080
Jonathan Locke <jonathanl@telenav.com>
2021-04-08 11:17:09 -0700

    ✨ Add @LexakaiJavadoc(complete = true) tag

---
+++ src/main/java/com/telenav/lexakai/annotations/LexakaiJavadoc.java
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// © 2011-2021 Telenav, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.telenav.lexakai.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the Javadoc for a given class or interface should be considered complete in terms of Javadoc coverage
+ * analysis, even if it doesn't meet the heuristic criteria for completeness.
+ *
+ * @author jonathanl (shibo)
+ * @see <a href="https://telenav.github.io/lexakai/">Lexakai documentation</a>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface LexakaiJavadoc
+{
+    boolean complete();
+}
