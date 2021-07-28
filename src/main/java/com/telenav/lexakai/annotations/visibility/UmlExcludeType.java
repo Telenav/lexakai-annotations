0de7633454773353bab793d5a66380a9407439ad
jonathan <jon@thanlocke.com>
2021-07-27 21:16:30 -0700

    🍱 Update Lexakai  - Add @UmlExcludeType annotation  - Build kivakit lexakai docs to include on state-of-the-art.org  - Move component classes into kivakit-applications  - Exclude RegistryKey from lookup UML diagram using new annotation  - Upgrade to lexakai 0.9.9-SNAPSHOT

---
+++ src/main/java/com/telenav/lexakai/annotations/visibility/UmlExcludeType.java
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// © 2011-2021 Telenav, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package com.telenav.lexakai.annotations.visibility;
+
+import com.telenav.lexakai.annotations.associations.UmlRelation;
+import com.telenav.lexakai.annotations.repeaters.UmlExcludeTypeRepeater;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Excludes the annotated type from all UML diagrams.
+ *
+ * @author jonathanl (shibo)
+ * @see <a href="https://telenav.github.io/lexakai/">Lexakai documentation</a>
+ * @see UmlRelation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+@Repeatable(UmlExcludeTypeRepeater.class)
+public @interface UmlExcludeType
+{
+    Class<?>[] value() default {};
+}
